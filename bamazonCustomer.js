adcf625a7d8a2d48c02d97a8e8b7ffcff2aff2f2
Chirag K Shah <chiragkshah@Chirags-MBP.home>
2017-09-13 01:44:40 -0400

    added inquire package and prompted user for item id and qauntity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -10,18 +11,44 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
 	if (err) throw err;
-	console.log('connected as id ' + connection.threadId);
+	// console.log('connected as id ' + connection.threadId);
 
 });
 
-
-
-connection.query('SELECT item_id, product_name, price FROM products', function(err, res){
-	// console.log ("res", res);
-
-	for (var i = 0; i < res.length; i++) {
-		console.log("ID:", res[i].item_id, "     Price", res[i].price, "     Name:", res[i].product_name);
-	}
-});
+showProducts();
 
 connection.end();
+
+function showProducts() {
+	console.log("\nWelcome to Bamazon!!!!");
+	console.log("----------------------\n\nProducts:\n");
+
+	connection.query('SELECT item_id, product_name, price FROM products', function(err, res){
+		// console.log ("res", res);
+
+		for (var i = 0; i < res.length; i++) {
+			console.log("ID:", res[i].item_id, "     Price", res[i].price, "     Name:", res[i].product_name);
+		}
+
+		//prompt customer:
+
+		inquirer.prompt([
+            {
+                name: "product",
+                message: "Which Product would you like to buy? (please type id)"
+            },
+            {
+                name: "qauntity",
+                message: "How many would you like to buy?"
+            },
+        ]). then(function(answers){
+            checkInventory(answers.product, answers.qauntity);            
+        })
+	});
+}
+
+function checkInventory(productId, qauntityRequested) {
+	console.log("checkInventory", productId, qauntityRequested);
+
+}
+
